e1bbebd59a33d311234bfd1a22802d2a414a0fee
ErikBoesen <me@erikboesen.com>
2018-11-04 19:45:36 -0500

    Get table and script from page

--- read.py
+++ read.py
@@ -9,6 +9,8 @@ HOST = 'http://54.243.195.23'
 team_number = '11-1091'
 page = requests.get(HOST + '/team.php?team=' + team_number).content
 soup = BeautifulSoup(page, 'html.parser')
+main_table = soup.find('table', class_='CSSTableGenerator')
+script = soup.find_all('script')[-1]
 
-print(soup)
-#print(soup.prettify())
+#print(soup)
+print(soup.prettify())
