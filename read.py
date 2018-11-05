7ea38366b13e0c3b749d1ffd9cf6a479724d3f75
ErikBoesen <me@erikboesen.com>
2018-11-04 20:18:43 -0500

    Remove stuff from cpparser

--- read.py
+++ read.py
@@ -1,12 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-from pick import pick
-from urllib.parse import unquote
-import argparse
 
 HOST = 'http://54.243.195.23'
 
-team_number = '11-1091'
+
 page = requests.get(HOST + '/team.php?team=' + team_number).content
 soup = BeautifulSoup(page, 'html.parser')
 google_script = soup.find('script')
