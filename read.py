521372dd86c5b1beabcfc0c1aecd8d1cc2c19d10
ErikBoesen <me@erikboesen.com>
2018-11-04 20:18:26 -0500

    Add other stuff to page

--- read.py
+++ read.py
@@ -9,8 +9,14 @@ HOST = 'http://54.243.195.23'
 team_number = '11-1091'
 page = requests.get(HOST + '/team.php?team=' + team_number).content
 soup = BeautifulSoup(page, 'html.parser')
+google_script = soup.find('script')
 main_table = soup.find('table', class_='CSSTableGenerator')
-script = soup.find_all('script')[-1]
+chart_script = soup.find_all('script')[-1]
+chart = soup.find(id='chart_div')
 
 #print(soup)
-print(soup.prettify())
+#print(soup.prettify())
+print(google_script)
+print(main_table)
+print(chart_script)
+print(chart)
