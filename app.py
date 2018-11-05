d2aecdedf3f0fcd099f3837ff69b5044664f4840
ErikBoesen <me@erikboesen.com>
2018-11-04 20:55:25 -0500

    Add templating in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 import requests
 from bs4 import BeautifulSoup
@@ -7,25 +7,27 @@ import json
 app = Flask(__name__)
 
 CP_HOST = 'http://54.243.195.23'
+with open('teams.json', 'r') as f:
+    teams = json.load(f)
 
 @app.route('/')
 def hello_world():
-
-    with open('teams.json', 'r') as f:
-        teams = json.load(f)
-
+    content = []
     for number, name in teams.items():
         page = requests.get(CP_HOST + '/team.php?team=' + number).content
         soup = BeautifulSoup(page, 'html.parser')
-        google_script = soup.find('script')
         main_table = soup.find('table', class_='CSSTableGenerator')
         chart_script = soup.find_all('script')[-1]
         chart = soup.find(id='chart_div')
-
-        #print(soup)
-        #print(soup.prettify())
-        print(google_script)
-        print(main_table)
-        print(chart_script)
-        print(chart)
-    return "It worked!"
+        content.append({
+            'number': number,
+            'name': name,
+            'main_table': main_table,
+            'chart_script': chart_script,
+            'chart': chart,
+        })
+        #print(google_script)
+        #print(main_table)
+        #print(chart_script)
+        #print(chart)
+    return render_template('index.html', teams=content)
