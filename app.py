cea2ec18aee25d171a4ff06c0ec1f536a7201d4e
ErikBoesen <me@erikboesen.com>
2018-12-08 22:00:45 -0500

    Handle teams not having data

--- app.py
+++ app.py
@@ -15,28 +15,31 @@ with open('teams.json', 'r') as f:
 def hello_world():
     targets = request.args.get('teams')
     if targets:
-        targets = str(urlparse(targets)).split(',')
+        targets = urlparse(targets).path.split(',')
     else:
         targets = []
     teams = []
     for number in targets:
         page = requests.get(TEAM_PATH + number).content
         soup = BeautifulSoup(page, 'html.parser')
-        main_table = soup.find('table', class_='CSSTableGenerator')
-        table_data = soup.find_all('tr')[1]
-        play_time = table_data.select('td')[6].text
-        score = table_data.select('td')[8].text
-        warnings = table_data.select('td')[7].text
-        chart_script = soup.find_all('script')[-1]
-        teams.append({
-            'number': number,
-            'name': names.get(number),
-            'play_time': play_time,
-            'score': score,
-            'warnings': warnings,
-            'main_table': main_table,
-            'chart_script': str(chart_script).replace('chart_div', 'chart_div_' + number),
-        })
+        try:
+            main_table = soup.find('table', class_='CSSTableGenerator')
+            table_data = soup.find_all('tr')[1]
+            play_time = table_data.select('td')[6].text
+            score = table_data.select('td')[8].text
+            warnings = table_data.select('td')[7].text
+            chart_script = soup.find_all('script')[-1]
+            teams.append({
+                'number': number,
+                'name': names.get(number),
+                'play_time': play_time,
+                'score': score,
+                'warnings': warnings,
+                'main_table': main_table,
+                'chart_script': str(chart_script).replace('chart_div', 'chart_div_' + number),
+            })
+        except:
+            continue
     teams = sorted(teams, key=lambda team: team['score'], reverse=True)
     return render_template('index.html', team_path=TEAM_PATH, teams=teams)
 
